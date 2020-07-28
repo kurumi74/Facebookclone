76c7904bcdb577d37854f4ec95dd959bbf74c64f
kurumi <krkrteikoku@gmail.comi>
2020-07-28 23:07:31 +0900

    CRUD機能、ログイン機能実装

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+    else
+      flash[:danger] = 'ログインに失敗しました'
+      render :new
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
+  end
+
+  def authenticate_user
+    if @current_user == nil
+      flash[:notice] = t('notice.login_needed')
+      redirect_to new_session_path
+    end
+  end
+end
